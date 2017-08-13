4cc2f11e725b9772e80842cde660ace9e7a92582
Ryan Reece <ryan.d.reece@live.com>
2017-08-12 23:18:21 -0700

    cp_around.sh patches

--- philosophy-reading-list/scripts/markdown2bib.py
+++ philosophy-reading-list/scripts/markdown2bib.py
@@ -136,25 +136,33 @@ def main():
         incollections = []
         miscs = []
         errors = []
+        duplicates = []
         allcitations = []
+        lowallcitations = []
 
         ## count the articles, books, ...
         for k in keys:
             ctype, citation, bibtex = results[k]
-            if ctype == 'article':
-                articles.append(citation)
-                allcitations.append(citation)
-            elif ctype == 'book':
-                books.append(citation)
-                allcitations.append(citation)
-            elif ctype == 'incollection':
-                incollections.append(citation)
-                allcitations.append(citation)
-            elif ctype == 'misc':
-                miscs.append(citation)
-                allcitations.append(citation)
+            low_citation = citation.lower()
+            if not low_citation in lowallcitations:
+                lowallcitations.append(low_citation)
+                if ctype == 'article':
+                    articles.append(citation)
+                    allcitations.append(citation)
+                elif ctype == 'book':
+                    books.append(citation)
+                    allcitations.append(citation)
+                elif ctype == 'incollection':
+                    incollections.append(citation)
+                    allcitations.append(citation)
+                elif ctype == 'misc':
+                    miscs.append(citation)
+                    allcitations.append(citation)
+                else:
+                    errors.append(k)
             else:
-                errors.append(k)
+                duplicates.append(k)
+                print 'Warning, duplicate: %s' % k
 
         print ''
 
@@ -169,6 +177,7 @@ def main():
         print '- %3i books' % len(books)
         print '- %3i miscs' % len(miscs)
         print '- %3i errors' % len(errors)
+        print '- %3i duplicates' % len(duplicates)
         print ''
 
         ## write header
